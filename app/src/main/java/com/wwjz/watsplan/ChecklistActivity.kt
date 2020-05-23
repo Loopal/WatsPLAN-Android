3213f2075e37c6c1c8c8bdf0e2eac6834faba1a1
Wenjiu Wang <vanjor1014@gmail.com>
2020-05-22 23:40:49 -0400

    added button logic

---
+++ app/src/main/java/com/wwjz/watsplan/ChecklistActivity.kt
@@ -0,0 +1,12 @@
+package com.wwjz.watsplan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChecklistActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checklist)
+    }
+}
