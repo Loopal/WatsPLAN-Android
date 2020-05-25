64a74d8f6711ac56a8f99b90275bb7dfb14718a2
Wenjiu Wang <vanjor1014@gmail.com>
2020-05-25 16:55:20 -0400

    database rule

--- app/src/main/java/com/wwjz/watsplan/Major.kt
+++ app/src/main/java/com/wwjz/watsplan/Major.kt
@@ -1,9 +1,5 @@
 package com.wwjz.watsplan
 
 data class Major(
-    val Communication1: List<String>? = listOf<String>(),
-    val Communication2: List<String>? = listOf<String>(),
-    val mFixed: List<String>? = listOf<String>(),
-    val mFlexible: List<String>? = listOf<String>(),
-    val sFixed: List<String>? = listOf<String>()
+    val Requirements: List<String>? = listOf<String>()
 )
